c6ba62ce44e0f4247156d663cfef551456c5225b
po043 <po04304a@naver.com>
2022-03-25 22:00:25 +0900

    test

--- anything.py
+++ anything.py
@@ -1,11 +1,22 @@
-max =0
-for _ in range(2):
-    sum=0
-    a = []
-    a = input().split(" ")
-    for i in a:
-        sum+=int(i)
-        if(sum > max):
-            max = sum
+Flag = True
 
-print(max)
+
+while Flag:
+    sum =0
+    a= input()
+    lenNum= len(a) +1
+    stra = str(a)
+    if a!='0':
+        for i in stra:
+            if i == "1":
+                sum +=2
+            elif i =="0":
+                sum +=4
+            else:
+                sum+= 3
+        sum +=lenNum
+        print(sum)
+    else:
+        Flag=False
+
+
---
+++ boj1075.py
@@ -0,0 +1,2 @@
+n = int(input())
+f = int(input())
